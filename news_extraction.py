ccd48a61f71a9745020abfce216aae592e014c8f
tracy3057 <68813553+tracy3057@users.noreply.github.com>
2023-06-07 00:18:39 -0500

    Update news_extraction.py

--- news_extraction.py
+++ news_extraction.py
@@ -26,15 +26,18 @@ except:
 
 # '(Russian AND ukraine AND war) OR (Russian AND ukraine AND conflict) OR (Russian AND ukraine AND invasion) OR (Russophobia) OR (Russophobic ) OR (RussiaPhobia) OR ("Demonise Russia") OR ("stand with Russia") OR ("I stand with Russia") OR ("I stand with Putin") OR (Nazi AND ukraine) OR (Denazify AND Ukraine) OR (Ukraine AND Nazis) OR (Ukraine AND Terrorist AND State) OR (Azov AND Nazi) OR (Minsk AND Accords) OR (Minsk AND Agreements) OR (Zelensky AND Regime) OR (Zelensky AND War AND Criminal) OR (NATO AND Russia AND War) OR (NATO AND War AND Crimes) OR (Protest AND NATO) OR (No AND To AND NATO) OR (Western AND hegemony) OR (US AND neocons) OR ("Biden War") OR ("global South" AND Russia) OR (BRICS AND G7) OR ("BRICS bloc") OR (Prague AND anti-Russian) OR ("Ukrainian Orthodox Church") OR (Minsk AND Merkel)'
 
+def get_data(url, page):
+    resp = requests.get(f"{ALL_URL}&size=100&page={str(page)}")
+    all_info = resp.json()
+    result_cnt = all_info['numResults']
+    return all_info, result_cnt
+    
 
 ALL_URL = f'https://api.goperigon.com/v1/all?apiKey={args.api_key}&from={args.start_time}&to={args.end_time}&sourceGroup=top10&source=eautocheck.de&showNumResults=true&showReprints=false&excludeLabel=Non-news&excludeLabel=Opinion&excludeLabel=Paid News&excludeLabel=Roundup&excludeLabel=Press Release&sortBy=date&source=nytimes.com&source=oryxspioenkop.com&source=mate.substack.com&source=wsj.com&source=pravda.com&source=reuters.com&source=thetimes.co.uk&source=bbc.co.uk&source=cnn.com&source=rt.com&source=reddit.com&source=youtube.com&source=theguardian.com&q={args.keywords}'
 # print(all_info.keys())
 while True:
     try:
-        resp = requests.get(f"{ALL_URL}&size=100&page=0")
-        all_info = resp.json()
-        result_cnt = all_info['numResults']
-        print(args.start_time)
+        all_info, result_cnt = get_data(ALL_URL,0)
         break
     except:
         print("I'm sleeping...")
@@ -49,15 +52,13 @@ if result_cnt > 100:
     while current_cnt > 0:
         while True:
             try:
-                resp = requests.get(f"{ALL_URL}&size=100&page={next_page}")
-                all_info = resp.json()
-                all_info['numResults'] # to check the validity of request
+                all_info, result_cnt = get_data(ALL_URL, next_page)
                 file_name = './test/' + args.start_time + '_' + args.end_time + '_page' + str(next_page) + '_total' + str(result_cnt) + '.json'
                 with open(file_name, "w") as f:
                     json.dump(all_info, f)
                 next_page += 1
                 current_cnt -= 100
-                print(args.start_time)
+#                 print(args.start_time)
                 break
             except:
                 print("I'm sleeping...")
